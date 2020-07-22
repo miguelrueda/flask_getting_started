b29b4e5122a214a3a3129c1ff9b190cedded8105
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-07-21 20:28:01 -0500

    Finished module 3 & 4

--- flashcards.py
+++ flashcards.py
@@ -1,20 +1,43 @@
-from flask import Flask
-from datetime import datetime
+"""
+Course ref https://github.com/codesensei-courses/flask-getting-started
+"""
+
+from flask import Flask, render_template, abort, jsonify, request
+from model import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to my Flash Cards application!"
+    return render_template("welcome.html", cards=db)
 
-@app.route("/date")
-def date():
-    return "This page was served at " + str(datetime.now())
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index, max_index = len(db)- 1)
+    except IndexError:
+        abort(404)
 
-counter = 0
+@app.route("/api/card/<int:index>")
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
 
-@app.route("/count_views")
-def count_views():
-    global counter
-    counter += 1
-    return "This page was served " + str(counter) + " times"
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
+
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card():
+    # if request.method == "POST":
+    #     # form has been submited
+    #     card = {
+    #         "question": request.form["question"],
+    #         "answer": request.form["answer"]
+    #     }
+    #     db.append(card)
+    # else:
+    return render_template("add_card.html")
